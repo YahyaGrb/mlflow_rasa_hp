5e1c32ed2987b9526d026281ec170900e4082961
YahyaGrb <71591581+YahyaGrb@users.noreply.github.com>
2023-01-06 23:27:15 +0100

    correct wrong commit on train.py
    commited test.py data in last commit

--- train.py
+++ train.py
@@ -1,69 +1,86 @@
-"""
-This script will serve to run the tests on the created model
-
-"""
-
-import json
-import time
 import tempfile
-import asyncio
 import mlflow
-
-from rasa.model_testing import test_nlu
-
-def extract_metric(file, metric_name):
-    with open(file, "r") as f:
-        metrics = json.load(f)
-
-    return metrics["weighted avg"][metric_name]
-
-
-# get_diet_config = lambda config: [
-#     component
-#     for component in config["pipeline"]
-#     if component["name"] == "DIETClassifier"][0]
-
-
-# import Path
-
 import click
-import os
-# @click.command()
-# @click.option("--model_path", help="Path readable by Spark to the ratings Parquet file")
-# @click.option("--validation", help="Path readable by Spark to the ratings Parquet file")
-def train_and_evaluate_nlu_model(model_path, validation):
-    mlflow.set_experiment("Tests")
+import time
+
+
+from rasa.shared.data import TrainingType
+from rasa.model_training import train_nlu
+
+
+def personnalised_trainer(config, training, training_type=TrainingType.NLU): #for future use only
+    from rasa.shared.importers.importer import TrainingDataImporter
+    from rasa.engine.recipes.recipe import Recipe
+    from rasa.model_training import (
+        DaskGraphRunner,
+        GraphTrainer, # really faster using cache
+        LocalTrainingCache,
+        Path,
+        _create_model_storage,
+        _determine_model_name,
+    )
     start_time = time.time()
+    file_importer = TrainingDataImporter.load_from_config(
+            config_path = config, training_data_paths = training
+    )
+    configuration = file_importer.get_config()
+    recipe = Recipe.recipe_for_name(configuration.get("recipe"))
+    model_configuration = recipe.graph_config_for_recipe(
+        configuration,
+        cli_parameters={},
+        training_type=training_type,
+    )
 
-    with tempfile.TemporaryDirectory() as temp_results_dir:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(
-            test_nlu( # faster than both compute_metrics, run_evaluation
-            model=model_path,
-            nlu_data=validation,
-            output_directory=temp_results_dir,
-            additional_arguments={},
-            ))
-        support = extract_metric(f"{temp_results_dir}/intent_report.json","support")
-        f1_intent = extract_metric(f"{temp_results_dir}/intent_report.json","f1-score")
-        f1_entity = extract_metric(f"{temp_results_dir}/DIETClassifier_report.json","f1-score")
-        mlflow.log_artifacts(temp_results_dir,  artifact_path="reports")
+    with tempfile.TemporaryDirectory() as temp_model_dir:
+        model_storage = _create_model_storage(
+            is_finetuning=False,
+            model_to_finetune=None,
+            temp_model_dir=Path(temp_model_dir),
+        )
+        cache = LocalTrainingCache()
+        trainer = GraphTrainer(model_storage, cache, DaskGraphRunner)
 
-    end_time = time.time()
-    print(f"Training time: {end_time - start_time}")
-    mlflow.log_metric("support", support)
-    mlflow.log_metric("f1-intent", f1_intent)
-    mlflow.log_metric("f1-entity", f1_entity)
-    mlflow.log_metric("duration", end_time - start_time)
-    return {
-        "support": support,
-        "f1_intent": f1_intent,
-        "f1_entity": f1_entity,
-        "elapsed_time": end_time - start_time,
-    }
+        model_name = _determine_model_name(
+            fixed_model_name=None, training_type=training_type
+        )
+        full_model_path = Path(temp_model_dir, model_name)
+        trainer.train(
+            model_configuration,
+            file_importer,
+            full_model_path,
+            force_retraining=False,
+            is_finetuning=False,
+        )
+        duration = time.time() - start_time
+        print(f"Training time: {duration}")
+        mlflow.log_artifact(model_path, artifact_path="model")
+        return duration
 
-if __name__=="__main__":
-    model_path = "mlruns/510145974657371863/4c944a69c2634bccb27cec407d2cf085/artifacts/model"
-    validation = "test_data.yml"
-    train_and_evaluate_nlu_model(model_path, validation)
+def fast_trainer(config, training):
+    mlflow.set_experiment("Trains")
+    with tempfile.TemporaryDirectory() as temp_model_dir:
+        start_time = time.time()
+        model_path = train_nlu(config=config, nlu_data=training, output=temp_model_dir, additional_arguments = {}) #faster
+        duration =  time.time() - start_time
+        print(f"Training time: {duration}")
+        mlflow.log_artifact(model_path, artifact_path="model")
+        return duration
+
+
+
+# @click.command()
+# @click.option("--config", help="Path readable by Spark to the ratings Parquet file")
+# @click.option("--training", help="Path readable by Spark to the ratings Parquet file")
+def train(config, training):
+    duration = fast_trainer(config, training)
+    mlflow.log_metric("train_duration", duration)
+    mlflow.log_artifact(config, artifact_path="config")
+
+if __name__ == '__main__':
+    # train()
+    # for manual execution uncomment following and comment preceding
+    config_file_path = "config.yml"
+    training_files = "training_data.yml"
+
+    train(config_file_path,training_files) # replace with train_nlu and see which is faster
+    print("train completed")
