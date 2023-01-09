58cbffa73922840d3f0b82aeea84838431020227
Yahya Ghrab <yghrab@dial-once.com>
2023-01-09 12:56:58 +0100

    Adding docstrings

--- hyperopt_rasa/main.py
+++ hyperopt_rasa/main.py
@@ -1,4 +1,4 @@
-from hyperopt import fmin, tpe, rand, space_eval
+from hyperopt import fmin, tpe, rand
 from space import search_space
 import os
 import time
@@ -10,8 +10,6 @@ import click
 from urllib.parse import urlparse
 
 from concurrent.futures import ThreadPoolExecutor
-
-import mlflow.projects
 from mlflow.tracking import MlflowClient
 
 _inf = np.finfo(np.float64).max
@@ -21,7 +19,9 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def _get_or_run(entrypoint, parameters, run_id, experiment_id, synchronous=True): #removed git commit and use_cache=True
-    logger.info("Launching new run for entrypoint={} and parameters={}".format(entrypoint, parameters))
+    logger.info(
+        f"Launching new run for entrypoint={entrypoint} and parameters={parameters}"
+    )
     submitted_run = mlflow.projects.run(".", entrypoint, parameters=parameters, env_manager="local", synchronous = synchronous, run_id=run_id, experiment_id=experiment_id)
     succeeded = submitted_run.wait()
     return MlflowClient().get_run(submitted_run.run_id)
@@ -49,37 +49,35 @@ def _transform_uri_to_path(uri):
 @click.option("--algo", type=click.STRING, default="tpe.suggest", help="Optimizer algorithm.")
 def workflow(config_template, train_data, validation_data,max_runs, metric, algo):
     """
-    Run hyperparameter optimization.
+    Hyperparameter search with Hyperopt library.
+    Args:
+        config_template: Path to the template configuration file.
+        train_data: Path to the training data file.
+        validation_data: Path to the validation data file.
+        max_runs: Maximum number of runs to evaluate.
+        metric: Metric to optimize on.
+        algo: Optimizer algorithm.
+    Return:
+        None
     """
-    # create random file to store run ids of the training tasks
-    # tracking_client = MlflowClient()
+
     start_time = time.time()
 
     def new_eval(experiment_id):
         """
         Create a new eval function
-
-        :param nepochs: Number of epochs to train the model.
         :experiment_id: Experiment id for the training run
-        :valid_null_loss: Loss of a null model on the validation dataset
-        :test_null_loss: Loss of a null model on the test dataset.
-        :return_test_loss: Return both validation and test loss if set.
-
         :return: new eval function.
         """
         def eval(space):
             """
-            Train Keras model with given parameters by invoking MLflow run.
-
-            Notice we store runUuid and resulting metric in a file. We will later use these to pick
-            the best run and to log the runUuids of the child runs as an artifact. This is a
-            temporary workaround until MLflow offers better mechanism of linking runs together.
-
-            :param params: Parameters to the train_keras script we optimize over:
-                          learning_rate, drop_out_1
-            :return: The metric value evaluated on the validation data.
+            Train then evaluate ml flows run with hyperopt optimization according to a search space.
+            Args:
+                space (Dict): hyper opt search space
+            Return:
+                test_loss (Float): test loss for the metric specified
             """
-            import mlflow.tracking
+
             with mlflow.start_run(nested=True) as child_run:
                 logger.info(f"Search space: {space}")
 
@@ -109,13 +107,6 @@ def workflow(config_template, train_data, validation_data,max_runs, metric, algo
                 test_loss = 1 - metrics[metric]
                 mlflow.log_params(params = space)
             return test_loss
-
-            # if succeeded:
-            #     training_run = tracking_client.get_run(p.run_id)
-            #     # metrics = training_run.data.metrics
-            # else:
-            #     # run failed => return null loss
-            #     tracking_client.set_terminated(p.run_id, "FAILED")
         return eval
 
     with mlflow.start_run() as run:
@@ -123,7 +114,7 @@ def workflow(config_template, train_data, validation_data,max_runs, metric, algo
         # activate multithreading
         runs = [(np.random.uniform(1e-5, 1e-1), np.random.uniform(0, 1.0)) for _ in range(max_runs)]
         with ThreadPoolExecutor(max_workers=max_runs) as executor: # use max_runs for number of workers/threads
-            best = executor.map(
+            executor.map(
                 fmin(
                     fn=new_eval(experiment_id),
                     space=search_space,
@@ -135,8 +126,7 @@ def workflow(config_template, train_data, validation_data,max_runs, metric, algo
             # find the best run, log its metrics as the final metrics of this run.
             client = MlflowClient()
             runs = client.search_runs(
-                [experiment_id], "tags.mlflow.parentRunId = '{run_id}' ".format(run_id=run.info.run_id)
-            )
+                [experiment_id], f"tags.mlflow.parentRunId = '{run.info.run_id}' ")
             best_test_loss = _inf
             best_run = None
             for r in runs:
@@ -153,7 +143,7 @@ def workflow(config_template, train_data, validation_data,max_runs, metric, algo
                     "Best_{}".format(metric): best_test_loss,
                 }
             )
-    print(f"Total execution time: {time.time() - start_time} seconds")
+    logger.info(f"Total execution time: {time.time() - start_time} seconds")
 
 
 
