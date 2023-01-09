283897cc7ea703b3f9eb2c22f74337ab51563c14
Yahya Ghrab <yghrab@dial-once.com>
2023-01-09 11:49:45 +0100

    simpler and improved train + docstrings

--- hyperopt_rasa/train.py
+++ hyperopt_rasa/train.py
@@ -2,12 +2,27 @@ from rasa.model_training import train_nlu
 
 import tempfile
 import mlflow
-import mlflow.projects
 import click
 import time
 
 
-def fast_trainer(config, training):
+
+
+@click.command()
+@click.option("--config", help="Path pointing to the config.yml file")
+@click.option("--training", help="Path pointing to nlu training yml file")
+def train(config, training):
+    """
+    Trains a rasa NLU model using the given configuration and training file path.
+    It also logs in mlflow the training duration as a metric and the model as an artifact.
+
+    Args:
+        config (string): Path to the config file (config.yml) for NLU.
+        training (string): Path to the NLU training data (training_data.yml).
+    Returns:
+        None
+    """
+
     with tempfile.TemporaryDirectory() as temp_model_dir:
         start_time = time.time()
         model_path = train_nlu(
@@ -15,27 +30,13 @@ def fast_trainer(config, training):
             nlu_data=training,
             output=temp_model_dir,
             additional_arguments={},
-        )  # faster
+        )
         duration = time.time() - start_time
-        print(f"Training time: {duration}")
+        print(f"Train completed.\nTraining time: {duration} s")
+        mlflow.log_metric("train_duration", duration)
         mlflow.log_artifact(model_path, artifact_path="model")
-        return duration
-
-
-@click.command()
-@click.option("--config", help="Path pointing to the config.yml file")
-@click.option("--training", help="Path pointing to nlu training yml file")
-def train(config, training):
-    duration = fast_trainer(config, training)
-    mlflow.log_metric("train_duration", duration)
-    mlflow.log_artifact(config, artifact_path="config")
+        mlflow.log_artifact(config, artifact_path="config")
 
 
 if __name__ == "__main__":
     train()
-    # # for manual execution uncomment following and comment preceding
-    # config_file_path = "../files/config.yml"
-    # training_files = "../files/training_data.yml"
-
-    # train(config_file_path,training_files) # replace with train_nlu and see which is faster
-    # print("train completed")
