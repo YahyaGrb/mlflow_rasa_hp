e0f6e2c981ae0e03e50b42a77a43df9fabbbebe5
Yahya Ghrab <yghrab@dial-once.com>
2023-01-09 17:35:57 +0100

    update search space

--- hyperopt_rasa/space.py
+++ hyperopt_rasa/space.py
@@ -5,5 +5,5 @@ from hyperopt.pyll.base import scope
 search_space = {
     "epochs": scope.int(hp.quniform("epochs", 0, 50,2)),
     "model": hp.choice("model", ["fr_core_news_sm", "fr_core_news_md", "fr_dep_news_trf"]),
-    "threshold": hp.uniform("threshold", 0, 1)
+    "threshold": hp.uniform("threshold", 0, 0.6)
     }
